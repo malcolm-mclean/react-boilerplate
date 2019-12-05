e0a9b1884649c812111042027af1f236970675ba
malcolm-mclean <mclean.malcolm.a@gmail.com>
2019-12-04 19:21:07 -0700

    Add initial app files

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const App = () => {
+	return <h1>This is just the beginning</h1>;
+};
+
+ReactDOM.render(
+	<App />,
+	document.getElementById("app")
+);
