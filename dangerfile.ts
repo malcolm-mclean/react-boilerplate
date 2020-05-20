aa6af58fa81b722c8def938c271cfd43b6ce450b
Malcolm McLean <mclean.malcolm.a@gmail.com>
2020-05-19 23:49:52 -0600

    Add danger.js and github actions to compare bundle size on PR

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,9 +1,27 @@
-import { markdown } from 'danger';
+import { markdown, warn } from 'danger';
 import report from './report.json';
 import baseReport from './base-report.json';
 
-const sizeToKilobyteString = (size: number) => {
-	const kilobytes = size / 1000;
+interface Bundle {
+	label: string;
+	statSize: number;
+	parsedSize: number;
+	gzipSize: number;
+	// group intentionally left out
+}
+
+const translateSize = (size: number) => {
+	if (size < 1000) {
+		return `${size} B`;
+	}
+
+	if (size > 1000000) {
+		const megabytes = (size / 1000000).toFixed(2);
+
+		return `${megabytes} MB`;
+	}
+
+	const kilobytes = (size / 1000).toFixed(2);
 
 	return `${kilobytes} KB`;
 };
@@ -15,51 +33,145 @@ const cleanBundleLabel = (label: string) => {
 	return label.slice(nameStartIndex, nameEndIndex);
 };
 
-const createRowsMarkdown = () => {
-	let rowsMarkdown = '';
+const reportToBundles = (report: any[]): Bundle[] => {
+	if (!Array.isArray(report)) {
+		fail('Bundle report is in an unexpected format');
+	}
 
-	report.map(bundle => {
-		const bundleName = cleanBundleLabel(bundle.label);
-		const gzipSize = sizeToKilobyteString(bundle.gzipSize);
-		const parsedSize = sizeToKilobyteString(bundle.parsedSize);
-
-		rowsMarkdown += `${bundleName} | ${gzipSize} | ${parsedSize}\n`;
+	const bundles: Bundle[] = report.map(bundle => {
+		return {
+			label: cleanBundleLabel(bundle.label),
+			statSize: bundle.statSize,
+			parsedSize: bundle.parsedSize,
+			gzipSize: bundle.gzipSize,
+		};
 	});
 
-	return rowsMarkdown.trim();
+	return bundles;
 };
 
-const createTableMarkdown = () => {
-	const headerMarkdown =
-		'**Bundle** | **Gzip Size** | **Parsed Size**\n--- | --- | ---\n';
+const compareSizeInBytes = (newSize: number, oldSize: number) => {
+	const sizeDiff = newSize - oldSize;
 
-	const rows = createRowsMarkdown();
-
-	markdown(`${headerMarkdown}${rows}`);
+	return translateSize(sizeDiff);
 };
 
-const createBaseRowsMarkdown = () => {
-	let rowsMarkdown = '';
+const compareSizeAsPercent = (newSize: number, oldSize: number) => {
+	const prefix = newSize > oldSize ? '+' : '-';
+	const decimal = Math.abs((newSize - oldSize) / oldSize);
+	const percent = (decimal * 100).toFixed(2);
 
-	baseReport.map(bundle => {
-		const bundleName = cleanBundleLabel(bundle.label);
-		const gzipSize = sizeToKilobyteString(bundle.gzipSize);
-		const parsedSize = sizeToKilobyteString(bundle.parsedSize);
+	return `${prefix}${percent}%`;
+};
 
-		rowsMarkdown += `${bundleName} | ${gzipSize} | ${parsedSize}\n`;
+const isSizeDiffSignificant = (newSize: number, oldSize: number) => {
+	const sizeDiff = Math.abs(newSize - oldSize);
+
+	return sizeDiff > 20;
+};
+
+const createSimpleRows = (bundles: Bundle[]) => {
+	let rows = '';
+
+	bundles.forEach(bundle => {
+		const bundleName = bundle.label;
+		const size = translateSize(bundle.statSize);
+		const minifiedSize = translateSize(bundle.parsedSize);
+		const gzipSize = translateSize(bundle.gzipSize);
+
+		rows += `${bundleName} | ${size} | ${minifiedSize} | ${gzipSize}\n`;
 	});
 
-	return rowsMarkdown.trim();
+	return rows.trim();
 };
 
-const createBaseTableMarkdown = () => {
-	const headerMarkdown =
-		'**Bundle** | **Gzip Size** | **Parsed Size**\n--- | --- | ---\n';
+const createSimpleBundleTable = (bundles: Bundle[], isAddingBundles = true) => {
+	const title = isAddingBundles
+		? '#### New bundles in this PR:'
+		: '#### Removed bundles in this PR:';
+	const tableHeader =
+		'Bundle | Size | Minified | Gzipped\n--- | --- | --- | ---\n';
+	const tableRows = createSimpleRows(bundles);
 
-	const rows = createBaseRowsMarkdown();
-
-	markdown(`${headerMarkdown}${rows}`);
+	markdown(title);
+	markdown(`${tableHeader}${tableRows}`);
 };
 
-createBaseTableMarkdown();
-createTableMarkdown();
+const createComparisonRows = (newBundles: Bundle[], oldBundles: Bundle[]) => {
+	let rows = '';
+
+	newBundles.forEach(bundle => {
+		const matchingBundle = oldBundles.find(ob => ob.label === bundle.label);
+
+		const newSize = bundle.gzipSize;
+		const oldSize = matchingBundle.gzipSize;
+
+		if (isSizeDiffSignificant(newSize, oldSize)) {
+			const bundleName = bundle.label;
+			const sizeDiff = compareSizeInBytes(newSize, oldSize);
+			const percentDiff = compareSizeAsPercent(newSize, oldSize);
+			const oldSizeString = translateSize(oldSize);
+			const newSizeString = translateSize(newSize);
+
+			rows += `${bundleName} | ${sizeDiff} | ${percentDiff} | ${oldSizeString} | ${newSizeString}\n`;
+		}
+	});
+
+	return rows.trim();
+};
+
+const createComparisonBundleTable = (
+	newBundles: Bundle[],
+	oldBundles: Bundle[]
+) => {
+	const title = '### Bundles changed in this PR:';
+	const tableHeader =
+		'Bundle | Size Diff | % Diff | Old Size | New Size\n--- | --- | --- | --- | ---\n';
+	const tableRows = createComparisonRows(newBundles, oldBundles);
+
+	if (!tableRows) {
+		return markdown('No significant bundle size changes in this PR ✅');
+	}
+
+	markdown(title);
+	markdown(`${tableHeader}${tableRows}`);
+	markdown('_Size differences calculated using gzip bundle size_');
+};
+
+const interpretBundles = (newBundles: Bundle[], oldBundles: Bundle[]) => {
+	const newBundlesToCompare = newBundles.filter(nb =>
+		oldBundles.some(ob => ob.label === nb.label)
+	);
+	const bundlesOnlyInNew = newBundles.filter(
+		nb => !oldBundles.some(ob => ob.label === nb.label)
+	);
+	const bundlesOnlyInOld = oldBundles.filter(
+		ob => !newBundles.some(nb => nb.label === ob.label)
+	);
+
+	const noBundlesToReport =
+		!newBundlesToCompare.length &&
+		!bundlesOnlyInNew.length &&
+		!bundlesOnlyInOld.length;
+
+	if (noBundlesToReport) {
+		return warn('No bundles to report on');
+	}
+
+	if (newBundlesToCompare.length) {
+		createComparisonBundleTable(newBundlesToCompare, oldBundles);
+	}
+
+	if (bundlesOnlyInNew.length) {
+		createSimpleBundleTable(bundlesOnlyInNew);
+	}
+
+	if (bundlesOnlyInOld.length) {
+		createSimpleBundleTable(bundlesOnlyInOld, false);
+	}
+};
+
+const newBundles = reportToBundles(report);
+const oldBundles = reportToBundles(baseReport);
+
+interpretBundles(newBundles, oldBundles);
