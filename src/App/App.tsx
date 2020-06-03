1b4ceaf4ce7ab25831033fc56db26f032aa3d0da
malcolm-mclean <mclean.malcolm.a@gmail.com>
2020-06-02 19:54:52 -0600

    Just look at the bundle size difference

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,26 +1,7 @@
-import React, { Suspense, lazy } from 'react';
-import moment from 'moment';
-import { BrowserRouter, Route } from 'react-router-dom';
-
-const Wassup = lazy(() =>
-	import(/* webpackChunkName: "wassup" */ '../pages/Wassup/Wassup')
-);
+import React from 'react';
 
 const App = () => {
-	return (
-		<>
-			<h1 data-test="app">This is just the beginning: {moment.now()}</h1>
-			<BrowserRouter>
-				<Suspense
-					fallback={<p>Make me a real loading animation please!</p>}
-				>
-					<Route exact path="/wassup">
-						<Wassup />
-					</Route>
-				</Suspense>
-			</BrowserRouter>
-		</>
-	);
+	return <h1 data-test="app">This is just the beginning</h1>;
 };
 
 export default App;
