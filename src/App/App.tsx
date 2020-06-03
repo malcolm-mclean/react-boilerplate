307e827f8756c4e2428d88e66cd8f58cec2b7602
malcolm-mclean <mclean.malcolm.a@gmail.com>
2020-06-02 19:22:48 -0600

    This is really why you should never use moment

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,24 +1,9 @@
-import React, { Suspense, lazy } from 'react';
-import moment from 'moment';
-import { BrowserRouter, Route } from 'react-router-dom';
-
-const Wassup = lazy(() =>
-	import(/* webpackChunkName: "wassup" */ '../pages/Wassup/Wassup')
-);
+import React from 'react';
 
 const App = () => {
 	return (
 		<>
-			<h1 data-test="app">This is just the beginning: {moment.now()}</h1>
-			<BrowserRouter>
-				<Suspense
-					fallback={<p>Make me a real loading animation please!</p>}
-				>
-					<Route exact path="/wassup">
-						<Wassup />
-					</Route>
-				</Suspense>
-			</BrowserRouter>
+			<h1 data-test="app">This is just the beginning</h1>
 		</>
 	);
 };
