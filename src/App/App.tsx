0b93b081952d2aff7c746f09889cea16f60c1f45
malcolm-mclean <mclean.malcolm.a@gmail.com>
2020-06-02 18:45:08 -0600

    Create stub component for demo

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import moment from 'moment';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Wassup = lazy(() =>
+	import(/* webpackChunkName: "wassup" */ '../pages/Wassup/Wassup')
+);
 
 const App = () => {
-	return <h1 data-test="app">This is just the beginning: {moment.now()}</h1>;
+	return (
+		<>
+			<h1 data-test="app">This is just the beginning: {moment.now()}</h1>
+			<BrowserRouter>
+				<Suspense
+					fallback={<p>Make me a real loading animation please!</p>}
+				>
+					<Route exact path="/wassup">
+						<Wassup />
+					</Route>
+				</Suspense>
+			</BrowserRouter>
+		</>
+	);
 };
 
 export default App;
---
+++ src/pages/Wassup/Wassup.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import moment from 'moment';
+
+const Wassup = () => {
+	return <h1>WAZZAAAAAAAAAAAAAAAAAAA {moment.now()}</h1>;
+};
+
+export default Wassup;
