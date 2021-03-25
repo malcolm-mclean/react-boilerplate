949d1c5e1b1f34e95f188acbe3358b46fdbbeddb
malcolm-mclean <mclean.malcolm.a@gmail.com>
2021-03-24 19:40:58 -0600

    Update Prettier and config

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,10 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
 
-describe('App', () => {
-	it('should render something', () => {
-		render(<App />);
+it('should render something', () => {
+	render(<App />);
 
-		expect(screen.getByTestId('app')).toBeTruthy();
-	});
+	expect(screen.getByTestId('app')).toBeTruthy();
 });
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,3 @@
-const App = () => {
+export const App = () => {
 	return <h1 data-test="app">This is just the beginning</h1>;
 };
-
-export default App;
