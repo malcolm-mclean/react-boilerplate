03da63912cf1236f180788b9fdead8e4cc317b72
malcolm-mclean <mclean.malcolm.a@gmail.com>
2021-07-18 17:33:33 -0600

    Add ignore patterns

--- scripts/report-test-coverage.ts
+++ scripts/report-test-coverage.ts
@@ -2,8 +2,19 @@ import { codeCoverage, PluginOptions } from 'danger-plugin-code-coverage';
 
 const defaultPluginOptions: PluginOptions[] = [
 	{
-		title: '### Code Coverage in this PR',
-		ignoreCoveragePattern: [],
+		title: 'Code Coverage in this PR',
+		ignoreCoveragePattern: [
+			'.js',
+			'.yml',
+			'.json',
+			'.md',
+			'.lock',
+			'.html',
+			'.config.',
+			'gitignore',
+			'prettier',
+			'report-'
+		],
 		coverageFilesPath: 'coverage/coverage-final.json'
 	}
 ];
